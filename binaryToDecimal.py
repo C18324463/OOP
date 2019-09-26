2a00c051305e07ed44c1ae4957616422e783bf91
c18324463 <c18324463@mytudublin.ie>
2019-09-26 15:47:27 +0100

    Week 2 Lab part 2

--- binaryToDecimal.py
+++ binaryToDecimal.py
@@ -2,11 +2,11 @@ binary = input("Enter a binary number\n")
 
 i = 0
 integer = 0
-power = 0
+powers = 0
 
 while i < len(binary):
     if binary[i] == '1':
-        integer = integer + 2*power
+        integer = integer + 2*powers
     i = i + 1
-    power = power + 1
+    powers = powers + 1
 print(integer)
